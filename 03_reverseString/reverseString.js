1215b56c4579cb36a362054b8cf1537e8b1c6823
Matt <124236404+SensuBeanz@users.noreply.github.com>
2023-03-25 19:07:06 +1100

    Reverse string complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let splitString = str.split("");
+    let flipString = splitString.reverse();
+    let joinString = flipString.join("")
+    return joinString;
 };
 
 // Do not edit below this line
