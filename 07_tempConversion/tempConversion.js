1b205248b0c031bfa542189db08ded2dfe3c3937
Matt <124236404+SensuBeanz@users.noreply.github.com>
2023-03-26 13:04:17 +1100

    tempConversion Complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(toCels) {
+  return Math.round((toCels - 32) * (5 / 9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(toFahr) {
+  return Math.round(((toFahr * 9) / 5 + 32) * 10) / 10;
 };
 
 // Do not edit below this line
