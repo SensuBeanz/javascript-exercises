3266b4d688f04cc09f5bdd188381f038c83967f1
Matt <124236404+SensuBeanz@users.noreply.github.com>
2023-03-26 12:01:33 +1100

    sumAll completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(first, second) {
+    if (!Number.isInteger(first) || !Number.isInteger(second)) return "ERROR";
+    if (first < 0 || second < 0) return "ERROR";
+    if (first > second) {
+        let temp = second;
+        second = first;
+        first = temp;
+    }
+    let runNum = 0
+    for (let i = first; i <= second; i++) {
+       runNum += i;
+    }
+    return runNum
 };
 
 // Do not edit below this line
